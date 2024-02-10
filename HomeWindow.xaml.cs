988d97364b54ccc6a35de10c0d5d7ac67b4dd406
ElimeletCA <elimelet.dev@gmail.com>
2024-02-10 11:46:51 -0400

    En este punto se implemento una conexion de prueba con Azure SQL Database con la base de datos y la tabla cliente, solo a modo de prueba, la estructura final no debe quedar asi

---
+++ HomeWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using SIGC.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SIGC
+{
+    /// <summary>
+    /// Interaction logic for HomeWindow.xaml
+    /// </summary>
+    public partial class HomeWindow : Window
+    {
+        private static DataTable dt = new DataTable();
+
+        public HomeWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                DataSet ds = FCliente.GetAll();
+                dt = ds.Tables[0];
+                dgvClientes.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+    }
+}
