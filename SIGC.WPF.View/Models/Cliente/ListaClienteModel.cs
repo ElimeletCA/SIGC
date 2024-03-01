e0a4734a6fe112cda1e7f8a298f2227d957482b3
ElimeletCA <elimelet.dev@gmail.com>
2024-02-29 20:15:18 -0400

    Se arregló la estructura del modelo mvvm ahora si esta en el punto adecuado para la implementacion de la navegacion.

---
+++ SIGC.WPF.View/Models/Cliente/ListaClienteModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using SIGC.Data;
+
+
+namespace SIGC.WPF.View.Models.Cliente
+{
+    public class ListaClienteModel
+    {
+        public static DataTable GetClienteDataTable()
+        {
+            try
+            {
+                DataSet DataSetCliente = FCliente.GetAll();
+                if (DataSetCliente.Tables[0].Rows.Count > 0)
+                {
+                    return DataSetCliente.Tables[0];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
